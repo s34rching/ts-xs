8dca64261dcba5af7c86a4d535aa4a0116e22fc1
Ivan Katsenia <ivan.katsenya@gmail.com>
2024-09-11 21:43:24 +0200

    Resistor Color Duo

---
+++ src/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,19 @@
+import * as colorsMap from './color-map.json'
+
+interface ColorsMap {
+  [name: string]: number
+}
+
+const getResistorCode = (mixedColor: string): number => {
+  try {
+    const colors = mixedColor.split('-')
+    const [firstColor, secondColor] = colors
+    const code = `${(colorsMap as ColorsMap)[firstColor]}${(colorsMap as ColorsMap)[secondColor]}`
+
+    return Number.parseInt(code, 10)
+  } catch (error) {
+    throw new Error('Something went wrong')
+  }
+}
+
+console.log(getResistorCode('yellow-black'))
