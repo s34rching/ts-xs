3910f89fe70d236e13ecf2bb1c122b9e13319f51
Ivan Katsenia <ivan.katsenya@gmail.com>
2024-09-20 21:43:44 +0200

    Create basic version of 'Triangle'

---
+++ src/triangle/triangle.ts
@@ -0,0 +1,42 @@
+export interface ValidationData {
+  isTriangle: boolean
+  message: string | null
+}
+
+export const validateSides = (sides: number[]): ValidationData => {
+  if (sides.length < 3) {
+    return { isTriangle: false, message: "Not enough sides" }
+  }
+
+  if (sides.length > 3) {
+    return { isTriangle: false, message: "Too many sides" }
+  }
+
+  if (sides.includes(0)) {
+    return { isTriangle: false, message: "Triangle side cannot be less than 0" }
+  }
+
+  if (!((sides[0] + sides[1] > sides[2]) && (sides[1] + sides[2] > sides[0]) && (sides[0] + sides[2] > sides[1]))) {
+    return { isTriangle: false, message: "Impossible to compose triangle from provided values" }
+  }
+
+  return { isTriangle: true, message: null }
+}
+
+export const defineTriangleType = (sides: number[]): string | null => {
+  const validated = validateSides(sides)
+
+  if (validated.isTriangle) {
+    const sortedSides = sides.sort((a, b) => a - b)
+
+    if (sides[0] === sides[1] && sides[1] === sides[2]) {
+      return "Equilateral triangle"
+    } if (sortedSides[1] === sortedSides[0] || sortedSides[1] === sortedSides[2]) {
+      return "Isosceles triangle"
+    } else {
+      return "Scalene triangle"
+    }
+  } else {
+    return validated.message
+  }
+}
